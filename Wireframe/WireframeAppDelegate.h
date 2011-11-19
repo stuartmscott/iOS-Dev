67b4eb72d673053049b791b3b399adbce8f3f6eb
Kostadin Vodenicharov <kostadinvo@gmail.com>
2011-11-19 13:33:41 +0000

    Initial Commit

---
+++ Wireframe/WireframeAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  WireframeAppDelegate.h
+//  Wireframe
+//
+//  Created by Kostadin on 19/11/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WireframeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
