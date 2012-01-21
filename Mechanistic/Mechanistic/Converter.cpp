83193b7bba05ab6a69305dbe9f6f891be5e397f7
Kostadin Vodenicharov <kostadinvo@gmail.com>
2012-01-21 13:20:33 +0000

    intermediate commit

---
+++ Mechanistic/Mechanistic/Converter.cpp
@@ -0,0 +1,18 @@
+//
+//  Converter.cpp
+//  Mechanistic
+//
+//  Created by Kostadin on 21/01/2012.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+#include "Converter.h"
+#include "MeshNode.h"
+
+class Converter
+{
+private:
+    MeshNode* gear;
+    MeshNode* tile;
+};
---
+++ Mechanistic/Mechanistic/Converter.h
@@ -0,0 +1,7 @@
+//
+//  Converter.h
+//  Mechanistic
+//
+//  Created by Kostadin on 21/01/2012.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
